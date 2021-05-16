7be727d1feb8dd0e0f5206c81142fdbd6750379b
Melissa Brewer <melramirez727@gmail.com>
2021-05-15 20:14:38 -0500

    added number, alph, symbols strings

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,13 @@
 var generateBtn = document.querySelector("#generate");
-
+// var declaration
 var passwordLength;
 var confirmLower;
 var confirmUpper;
 var confirmNumber;
 var confirmSpecial;
 var userChoices;
-
+// strings
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbols = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
